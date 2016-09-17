a73db30f47b29fe8303606a20308b11066aeb36c
M. Hassan Khurshid <mkshid@gmail.com>
2016-09-18 00:14:29 +0200

    First try of creating a page like trello

--- packages/base/webpages/main_page.py
+++ packages/base/webpages/main_page.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 
+from gnr.core.gnrbag import Bag
+from gnr.core.gnrdecorator import public_method
+
 
 class GnrCustomWebPage(object):
+
+    css_requires = 'css/base'
 
     def main(self, root, **kwargs):
         frame = root.framePane()
@@ -13,7 +18,42 @@ class GnrCustomWebPage(object):
 
 
     def team_page(self, pane):
-        pass
+        qs =  self.get_teams_boards()
+        pane.data('^team', qs)
+
+        for r in qs:
+            team_id = r.getLabel()
+            values = r.getValue()
+            team_div = pane.div(
+                '^.{0}?team_name'.format(team_id),
+                _class='team-title'
+            ).ul(_class='board-list')
+
+            for v in values:
+                team_div.li(
+                    '^.{0}.{1}.name'.format(
+                        team_id,
+                        v.getValue().getItem('pkey')),
+                    connect_onclick='debugger;',
+                    _class='board-tile'
+                )
 
     def board_page(self, pane):
         pass
+
+
+    @public_method
+    def get_teams_boards(self):
+        tbl = self.db.table('base.board')
+        qs = tbl.query(
+            '$name,$team_id,$team_name,$position',
+            where='$owner_user_id=:user_id',
+            user_id=self.dbCurrentEnv()['user_id'],
+            order_by='$position'
+        ).fetch()
+
+        result = Bag()
+        for r in qs:
+            result.setAttr(r['team_id'], team_name=r['team_name'])
+            result.setItem('{0}.{1}'.format(r['team_id'], r['pkey']), Bag(r))
+        return result
