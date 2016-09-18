19255e40dbd9c234be4b87d199a1a1bae49392f0
M. Hassan Khurshid <mkshid@gmail.com>
2016-09-18 13:35:16 +0200

    Make a serverCall and set returned values to the datapath

--- packages/base/resources/js/main_page.js
+++ packages/base/resources/js/main_page.js
@@ -1,4 +1,17 @@
 function generate_board_page(that, board_id){
+    /* Generate the board page */
+
+    // Gets the node of board page
     var bp_node = that.nodeById('board_page');
-    
+
+    // Make a serverCall to gets lists and cards related
+    // to the board.
+    var result = genro.serverCall(
+        'get_lists_cards',
+        {board_id: board_id}
+    );
+
+    // sets the return result to the board datapath
+    that.setRelativeData('board', result);
+
 }
