5c4659bb4492d99b9025266608d6e3a270fb1836
M. Hassan Khurshid <mkshid@gmail.com>
2016-09-18 22:31:34 +0200

    Add some css

--- packages/base/resources/js/main_page.js
+++ packages/base/resources/js/main_page.js
@@ -16,9 +16,9 @@ function generate_board_page(that, board_id){
     var res_asDict = result.asDict(true);
 
     for (var k in res_asDict){
-         var list_div = bp_node._('div');
-         // name of the list
-         list_div._('h3', {innerHTML: '^.' + k + '?list_name'});
+        var list_div = bp_node._('div', {_class:'list-wrapper'});
+        // name of the list
+        list_div._('h3', {innerHTML: '^.' + k + '?list_name'});
 
          // create a ul for the cards
          var ul = list_div._('ul');
