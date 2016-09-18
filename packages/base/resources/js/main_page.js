95a10dcfb19a1e065c63291d444f81f71097d134
M. Hassan Khurshid <mkshid@gmail.com>
2016-09-18 18:26:27 +0200

    Add a raw function to create a a div with list name and his cards

--- packages/base/resources/js/main_page.js
+++ packages/base/resources/js/main_page.js
@@ -10,8 +10,24 @@ function generate_board_page(that, board_id){
         'get_lists_cards',
         {board_id: board_id}
     );
-
     // sets the return result to the board datapath
     that.setRelativeData('board', result);
 
+    var res_asDict = result.asDict(true);
+
+    for (var k in res_asDict){
+         var list_div = bp_node._('div');
+         // name of the list
+         list_div._('h3', {innerHTML: '^.' + k + '?list_name'});
+
+         // create a ul for the cards
+         var ul = list_div._('ul');
+         var cards = res_asDict[k];
+         for (var c in cards){
+             ul._('li', {innerHTML: '^.' + k + '.' + c + '.name'});
+        }
+    }
+
+    // Once done with the rendering change the page
+    that.setRelativeData('page_selected', 1);
 }
