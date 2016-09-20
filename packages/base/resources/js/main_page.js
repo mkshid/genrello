7b537266a1e4c0c5372917ffbf9fec20eba4828f
M. Hassan Khurshid <mkshid@gmail.com>
2016-09-20 23:35:55 +0200

    Add style to the list

--- packages/base/resources/js/main_page.js
+++ packages/base/resources/js/main_page.js
@@ -16,8 +16,9 @@ function generate_board_page(that, board_id){
     var res_asDict = result.asDict(true);
 
     for (var k in res_asDict){
-        var list_div = bp_node._('div', {_class:'list-wrapper'});
+        var list_wrapper = bp_node._('div', {_class:'list-wrapper'});
         // name of the list
+        var list_div = list_wrapper._('div', {_class: 'gen-list'})
         list_div._('h3', {innerHTML: '^.' + k + '?list_name'});
 
          // create a ul for the cards
